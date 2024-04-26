bbf3b35f3f95006b0f28c8244e2a4b7f3deddda8
Bukunmi Kuti <bukunmikuti@gmail.com>
2024-04-26 17:08:22 +0100

    - feat: add Demo page - Chore: add PrimeVue dep

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,39 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  ssr: false,
+  devtools: { enabled: false },
+  typescript: {
+    shim: false,
+  },
+  css: ["@/assets/css/main.less"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        less: {
+          additionalData: `@import './assets/css/theme.less';`,
+        },
+      },
+    },
+  },
+  app: {
+    head: {
+      link: [
+        { rel: "preconnect", href: "https://fonts.googleapis.com" },
+        {
+          rel: "preconnect",
+          href: "https://fonts.gstatic.com",
+          crossorigin: "",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500&display=swap",
+        },
+      ],
+      meta: [
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { charset: "utf-8" },
+      ],
+    },
+  },
+  modules: ["nuxt-primevue"],
+});
---
+++ pages/dashboard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <Sidebar></Sidebar>
+    </div>
+</template>
+
+<script setup>
+import Sidebar from '~/components/Sidebar.vue';
+
+
+</script>
+
+<style scoped lang="less">
+
+</style>
